c8ede6b9879bbc32f72ffba9863bd4f5d9ff1004
jxl <738179011@qq.com>
2021-12-16 14:11:54 +0800

    完善代码

---
+++ dist/assets/iframe.e846d2b5.js
@@ -0,0 +1,1 @@
+import{d as a,b as o,e as s,o as i,q as c}from"./vendor.480a1782.js";const m=["height"],d=a({setup(h){const e=o(),t=o(0);return s(()=>{t.value=e.value.parentElement.clientHeight}),(l,p)=>(i(),c("iframe",{ref:(n,r)=>{r.myFrame=n,e.value=n},src:"http://qingqingxuan.gitee.io/work-p-site",width:"100%",height:t.value+"px",frameborder:"0"},null,8,m))}});export{d as default};
